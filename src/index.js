cb5180954f10e7dee1976c1849369302fb4ad5d5
haingdc <haingdc@gmail.com>
2018-02-04 21:39:19 +0700

    Adding state management

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import { enthusiasm } from "./reducers/index";
+
+const store = createStore < StoreState > (enthusiasm, {});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,22 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
-import Hello from "./components/Hello";
+import Hello from "./containers/Hello";
+import { createStore } from "redux";
+
+import { StoreState } from "./types/index";
+import { enthusiasm } from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: "TypeScript",
+});
 
 ReactDOM.render(
-  <Hello name="Typescript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById("root") as HTMLElement,
 );
 registerServiceWorker();
