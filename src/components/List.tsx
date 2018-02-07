f2bcd01c0cff6164df8b0e11b2e1ccc371e6a8b4
haingdc <haingdc@gmail.com>
2018-02-08 01:51:57 +0700

    fix icon is small

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -13,11 +13,11 @@ export default function List(props: Props) {
       <header>
         <svg className="hidden">
           <defs>
-            <symbol id="icon-search" viewBox="0 0 24 24">
+            <symbol id="icon-search" viewBox="0 0 14 14">
               <title>search</title>
               <path d="M13.707 12.293l-2.822-2.822c.699-.981 1.115-2.177 1.115-3.471 0-3.309-2.691-6-6-6s-6 2.691-6 6 2.691 6 6 6c1.294 0 2.49-.416 3.471-1.115l2.822 2.822c.195.195.451.293.707.293s.512-.098.707-.293c.391-.391.391-1.023 0-1.414zm-7.707-2.293c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4z" />
             </symbol>
-            <symbol id="icon-cross" viewBox="0 0 24 24">
+            <symbol id="icon-cross" viewBox="0 0 12 12">
               <title>cross</title>
               <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
             </symbol>
@@ -36,10 +36,10 @@ export default function List(props: Props) {
             <input type="text" />
             <span className="find-btn">
               {/* prettier-ignore */}
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14"><use xlinkHref="#icon-search"></use></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"><use xlinkHref="#icon-search"></use></svg>
             </span>
             {/* prettier-ignore */}
-            <span className="x" ><svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+            <span className="x" ><svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
               <use xlinkHref="#icon-cross"></use>
             </svg></span>
           </fieldset>
